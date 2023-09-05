306f4a7a0c375a5a67e48f71baa0b062552cd02a
gastonmartinezz <ezegaston2011@gmail.com>
2023-09-04 21:01:07 -0300

    Hecho codigo JS y HTML.
    Hecho el codigo de JS y HTML. Falta el diseño con CSS.

---
+++ index.js
@@ -0,0 +1,17 @@
+const kanye = "https://api.kanye.rest/";
+
+const p = document.querySelector('p');
+
+async function getKanyeQuote() {
+    try {
+        const response = await fetch(kanye);
+        const phrase = await response.json();
+    
+        p.textContent = phrase.quote;
+        
+    } catch (error) {
+        p.textContent = "There was a problem with the fetch operation"
+    }
+}
+
+getKanyeQuote();
